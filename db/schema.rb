88c05a2fc1d8cecd78ca545f666c5be399e505c5
Aadel Bouzambou <abouzambou@gmail.com>
2022-06-08 17:02:05 +0200

    column added to schema for a better seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_125018) do
+ActiveRecord::Schema.define(version: 2022_06_08_145920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_06_06_125018) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "character_picture"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
@@ -47,6 +48,9 @@ ActiveRecord::Schema.define(version: 2022_06_06_125018) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "profile_picture"
+    t.text "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
