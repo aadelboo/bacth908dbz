3276395238c59c1fc30b5c6b7ffbb38efb94df53
Aadel Bouzambou <abouzambou@gmail.com>
2022-06-13 17:37:55 +0200

    seed finished and test on local host

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# this is a comment to be commited
 
 
 puts "creating some seed"
